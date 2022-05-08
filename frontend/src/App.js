09354cfdfd3428088570662309f3517668a6a097
Sean Joseph <theseanjoseph@gmail.com>
2022-05-08 13:24:09 -0400

    Connected frontend to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,86 @@
 import './App.css';
-import { Container, Box, FormGroup, Button, Autocomplete, TextField, Stack, Grid } from '@mui/material'
+import { Container, Box, FormGroup, Button, Autocomplete, TextField, Grid, ButtonGroup, List, ListItem, ListItemText, Stack, Link } from '@mui/material'
 import { useState } from 'react';
+
+function searchParams(terms) {
+  return "?" + new URLSearchParams({terms: terms})
+}
+
+function suggestionUrl(terms) {
+  return "http://localhost:8000/suggestions" + searchParams(terms)
+}
+
+function searchResultsUrl(terms) {
+  return "http://localhost:8000/search-results" + searchParams(terms)
+}
+
+function exportSearchResultsUrl(terms) {
+  return "http://localhost:8000/search-results/xls" + searchParams(terms)
+}
 
 export default function App() {
   const [options, setOptions] = useState([]);
+  const [terms, setTerms] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+  const [exportUrl, setExportUrl] = useState([]);
 
   const onChangeFindOptions = async value => {
-    const response = await fetch("http://localhost:8000/suggestions?" + new URLSearchParams({terms: value}))
-    const suggestedOptions = await response.json()
-    setOptions(suggestedOptions)
+    const response = await fetch(suggestionUrl(value));
+    const suggestedOptions = await response.json();
+    setOptions(suggestedOptions);
+  }
+
+  const onSearchClickDoSearch = async value => {
+    const response = await fetch(searchResultsUrl(terms));
+    const results = await response.json();
+    setSearchResults(results);
   }
 
   return (
     <Container maxWidth="lg">
       <Box sx={{ my: 8 }}>
-        <FormGroup>
-          <Grid container spacing={2}>
-            <Grid item xs={8}>
-            <Autocomplete 
-              id="search-bar"
-              options={options}
-              renderInput={(params) => <TextField {...params} 
-                label="searchSuggestions"
-                onChange={ev => {
-                  if (ev.target.value !== "" || ev.target.value !== null) {
-                    onChangeFindOptions(ev.target.value);
-                  }
-                }}
-                />}
-              />
+        <Stack>
+          <FormGroup>
+            <Grid container spacing={2}>
+              <Grid item xs={8}>
+              <Autocomplete 
+                id="search-bar"
+                options={options}
+                renderInput={(params) => <TextField {...params} 
+                  label="No search terms yet..."
+                  onChange={ev => {
+                    setTerms(ev.target.value)
+                    if (ev.target.value !== "" || ev.target.value !== null) {
+                      onChangeFindOptions(ev.target.value);
+                      setExportUrl(exportSearchResultsUrl(ev.target.value))
+                    }
+                  }}
+                  />}
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <ButtonGroup>
+                  <Button id="search-button" onClick={ev => {
+                          onSearchClickDoSearch(terms)
+                        }} 
+                        variant="contained">
+                      Search
+                    </Button>
+                  <Button id="export-search-results" href={exportUrl} target="_blank">Export</Button>
+                </ButtonGroup>
+              </Grid>
             </Grid>
-            <Grid item xs={4}>
-              <Button id="search-button" variant="contained">Search</Button>
-              <Button id="export-search-results" variant="contained">Export</Button>
-            </Grid>
-          </Grid>
-        </FormGroup>
+          </FormGroup>
+          <List>
+                  {searchResults.map(result => {
+                    return (
+                      <ListItem>
+                        <ListItemText>{result.all}</ListItemText>
+                      </ListItem>
+                    )
+                  })}
+          </List>
+        </Stack>
       </Box>
 
     </Container>
